ab04982d63974fc0389e10d38a7b3faa614db974
Daniel Hahler <git@thequod.de>
2015-04-29 11:52:36 +0200

    pep8 fixes; sort imports

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import os
+import subprocess
 import urllib
 import zipfile
-import subprocess
 
 import pytest
 
@@ -16,14 +16,15 @@ class IntegrationTestFile(object):
         self.path = path
 
     def run(self):
-        output = subprocess.check_output([VSPEC_RUNNER, '.', VSPEC_FOLDER, self.path])
+        output = subprocess.check_output(
+            [VSPEC_RUNNER, '.', VSPEC_FOLDER, self.path])
         for line in output.splitlines():
             if line.startswith(b'not ok') or line.startswith(b'Error'):
                 pytest.fail("{} failed:\n{}".format(
                     self.path, output.decode('utf-8')), pytrace=False)
 
     def __repr__(self):
-        return "<%s: %s>"  % (type(self), self.path)
+        return "<%s: %s>" % (type(self), self.path)
 
 
 def pytest_configure(config):
@@ -38,7 +39,7 @@ def pytest_configure(config):
             dest = os.path.join(CACHE_FOLDER, n)
             destdir = os.path.dirname(dest)
             if not os.path.isdir(destdir):
-              os.makedirs(destdir)
+                os.makedirs(destdir)
             data = z.read(n)
             if not os.path.isdir(dest):
                 with open(dest, 'w') as f:
