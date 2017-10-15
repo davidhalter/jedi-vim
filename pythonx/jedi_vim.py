2d60a7efb4ad6bc0df5e5faa865fdeafc915651f
Dave Halter <davidhalter88@gmail.com>
2017-10-15 15:49:43 +0200

    Rename was in the wrong spot when using it at the end of a line.

--- pythonx/jedi_vim.py
+++ pythonx/jedi_vim.py
@@ -608,8 +608,14 @@ def rename():
         vim_command('augroup END')
 
         vim_command("let s:jedi_replace_orig = expand('<cword>')")
+        line = vim_eval('getline(".")')
         vim_command('normal! diw')
-        vim_command('startinsert')
+        if re.match('\w+$', line[cursor[1]:]):
+            # In case the deleted word is at the end of the line we need to
+            # move the cursor to the end.
+            vim_command('startinsert!')
+        else:
+            vim_command('startinsert')
 
     else:
         # Remove autocommand.
