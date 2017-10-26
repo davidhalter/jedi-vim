cc087ef463ba0cedf61db9ef75a821847e7390a1
Dave Halter <davidhalter88@gmail.com>
2017-10-27 00:07:33 +0200

    Somehow show_goto_multi_results was called without arguments
    This was obviously wrong and fixed by inserting the obvious param

--- pythonx/jedi_vim.py
+++ pythonx/jedi_vim.py
@@ -336,7 +336,7 @@ def goto(mode="goto"):
                                     repr(PythonToVimStr(old_wildignore)))
             vim.current.window.cursor = d.line, d.column
     else:
-        show_goto_multi_results()
+        show_goto_multi_results(definitions)
     return definitions
 
 
