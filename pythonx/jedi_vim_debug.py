2d61d6a568113c5912e10b1100979322e8fd1ff2
naronA <otherthan.new@gmail.com>
2017-12-28 17:18:04 +0900

    fix: CheckHealth doesn't work in neovim
    CheckHealth calls display_debug_info().
    However, error occured when loading jedi_vim.jedi.Script('')._evaluator.sys_path,
    because Evaluator.sys_path is unavailable variable.

--- pythonx/jedi_vim_debug.py
+++ pythonx/jedi_vim_debug.py
@@ -31,7 +31,7 @@ def display_debug_info():
             echo("printf(' - version: %s', {0!r})".format(
                 jedi_vim.jedi.__version__))
             echo("' - sys_path:'")
-            for p in jedi_vim.jedi.Script('')._evaluator.sys_path:
+            for p in jedi_vim.jedi.Script('')._evaluator.project.sys_path:
                 echo("printf('    - `%s`', {0!r})".format(p))
     except Exception as e:
         echo("printf('There was an error accessing jedi_vim.jedi: %s', "
